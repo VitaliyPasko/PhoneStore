320b1ea333e89a010c1ed45c75ec184fd2501714
TTrueBenji <mr.lolleague@mail.ru>
2022-06-20 21:42:28 +0600

    Реализовать функционал заказов.
    ***
    Реализовал возможность закза смартфона.
    ***

--- PhoneStore/Controllers/HomeController.cs
+++ PhoneStore/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using PhoneStore.Models;
 
@@ -8,10 +10,14 @@ namespace PhoneStore.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHostEnvironment _environment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(
+            ILogger<HomeController> logger, 
+            IHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
         
         [HttpGet]
@@ -20,6 +26,66 @@ namespace PhoneStore.Controllers
             return View();
         }
 
+        // [HttpGet]
+        // public IActionResult Test()
+        // {
+        //     string filePath = Path.Combine(_environment.ContentRootPath, "Files/test.pdf");
+        //     string fileType = "application/pdf";
+        //     string fileName = "test.pdf";
+        //     return PhysicalFile(filePath, fileType, fileName);
+        // }
+        
+        // [HttpGet]
+        // public IActionResult Test()
+        // {
+        //     string filePath = Path.Combine(_environment.ContentRootPath, "Files/test.pdf");
+        //     byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+        //     string fileType = "application/pdf";
+        //     string fileName = "test.pdf";
+        //     return File(bytes, fileType, fileName);
+        // }
+        
+        // [HttpGet]
+        // public IActionResult Test()
+        // {
+        //     string filePath = Path.Combine(_environment.ContentRootPath, "Files/test.pdf");
+        //     byte[] bytes = System.IO.File.ReadAllBytes(filePath);
+        //     string fileType = "application/pdf";
+        //     string fileName = "test.pdf";
+        //     return File(bytes, fileType, fileName);
+        // }
+        
+        // [HttpGet]
+        // public IActionResult Test()
+        // {
+        //     string filePath = Path.Combine(_environment.ContentRootPath, "Files/test.pdf");
+        //     FileStream stream = new FileStream(filePath, FileMode.Open);
+        //     string fileType = "application/pdf";
+        //     string fileName = "test.pdf";
+        //     return File(stream, fileType, fileName);
+        // }
+        
+        [HttpGet]
+        public IActionResult Test()
+        {
+            string filePath = Path.Combine("~/Files/", "test.txt");
+            string fileType = "application/txt";
+            return File(filePath, fileType);
+        }
+        
+        [HttpGet]
+        [ActionName("Test2")]
+        public string Test(Phone phone)
+        {
+            return "tets";
+        }
+        
+        [HttpGet]
+        [ActionName("Test3")]
+        public string Test(Phone[] phones)
+        {
+            return "tets";
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ PhoneStore/Controllers/OrdersController.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhoneStore.Models;
+
+namespace PhoneStore.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly MobileContext _db;
+
+        public OrdersController(MobileContext db)
+        {
+            _db = db;
+        }
+
+        // GET
+        public IActionResult Index()
+        {
+            var orders = _db.Orders
+                .Include(p => p.Phone)
+                .ToList();
+            
+            return View(orders);
+        }
+        
+        [HttpGet]
+        public IActionResult Create(int phoneId)
+        {
+            var phone = _db.Phones.FirstOrDefault(p => p.Id == phoneId);
+            if (phone is null)
+                return Content("Такого телефона нет.");
+            Order order = new Order
+            {
+                Phone = phone
+            };
+            
+            return View(order);
+        }
+        
+        [HttpPost]
+        public IActionResult Create(Order order)
+        {
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
