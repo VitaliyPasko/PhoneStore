a51fd677f582fb583edaf891622d6beedfa8c51a
Vitaliy Pasko <pasko.vitaliy24@gmail.com>
2022-08-08 22:29:16 +0600

    - вынес скрипт в файл. - поправил фронт.

--- PhoneStore/wwwroot/js/site.js
+++ PhoneStore/wwwroot/js/site.js
@@ -9,3 +9,18 @@
             .load(`https://localhost:5001/Account/SearchAccounts?searchTerm=${searchTerm}`);
     })
 });
+
+$(document).ready(function (){
+    $("#feedbackForm").on("submit", function (e){
+        e.preventDefault();
+        let phoneId = $("input[name=phoneId]");
+        let text = $("#text");
+        let feedback = {
+            text: text.val(),
+            phoneId: phoneId.attr('id')
+        };
+        $.post('https://localhost:5001/feedback/create', feedback, function (response){
+            $('#feedbacks').before(response);
+        });
+    });
+});
